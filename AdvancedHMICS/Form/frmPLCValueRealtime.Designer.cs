c358545ae6a38b1c07418c4fdb3316b4f23fb36c
securitylong100 <30890603+securitylong100@users.noreply.github.com>
2023-02-25 15:44:48 +0700

    update
    f

--- AdvancedHMICS/Form/frmPLCValueRealtime.Designer.cs
+++ AdvancedHMICS/Form/frmPLCValueRealtime.Designer.cs
@@ -31,37 +31,238 @@ namespace AdvancedHMICS
         {
             this.gc_main = new DevExpress.XtraGrid.GridControl();
             this.gv_main = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumn17 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn4 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn5 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn6 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn7 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn8 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn9 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn10 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn11 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn12 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn13 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn14 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn15 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn16 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.btn_reload = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.gc_main)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gv_main)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // gc_main
             // 
-            this.gc_main.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.gc_main.Location = new System.Drawing.Point(0, 99);
+            this.gc_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gc_main.Location = new System.Drawing.Point(3, 43);
             this.gc_main.MainView = this.gv_main;
             this.gc_main.Name = "gc_main";
-            this.gc_main.Size = new System.Drawing.Size(800, 351);
+            this.gc_main.Size = new System.Drawing.Size(794, 404);
             this.gc_main.TabIndex = 0;
             this.gc_main.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gv_main});
             // 
             // gv_main
             // 
+            this.gv_main.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumn17,
+            this.gridColumn1,
+            this.gridColumn2,
+            this.gridColumn3,
+            this.gridColumn4,
+            this.gridColumn5,
+            this.gridColumn6,
+            this.gridColumn7,
+            this.gridColumn8,
+            this.gridColumn9,
+            this.gridColumn10,
+            this.gridColumn11,
+            this.gridColumn12,
+            this.gridColumn13,
+            this.gridColumn14,
+            this.gridColumn15,
+            this.gridColumn16});
             this.gv_main.GridControl = this.gc_main;
             this.gv_main.Name = "gv_main";
+            // 
+            // gridColumn17
+            // 
+            this.gridColumn17.Caption = "Adress ";
+            this.gridColumn17.FieldName = "address_";
+            this.gridColumn17.Name = "gridColumn17";
+            this.gridColumn17.Visible = true;
+            this.gridColumn17.VisibleIndex = 0;
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.Caption = "Bit 15";
+            this.gridColumn1.FieldName = "bit_15";
+            this.gridColumn1.Name = "gridColumn1";
+            this.gridColumn1.Visible = true;
+            this.gridColumn1.VisibleIndex = 1;
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.Caption = "Bit 14";
+            this.gridColumn2.FieldName = "bit_14";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 2;
+            // 
+            // gridColumn3
+            // 
+            this.gridColumn3.Caption = "Bit 13";
+            this.gridColumn3.FieldName = "bit_13";
+            this.gridColumn3.Name = "gridColumn3";
+            this.gridColumn3.Visible = true;
+            this.gridColumn3.VisibleIndex = 3;
+            // 
+            // gridColumn4
+            // 
+            this.gridColumn4.Caption = "Bit 12";
+            this.gridColumn4.FieldName = "bit_12";
+            this.gridColumn4.Name = "gridColumn4";
+            this.gridColumn4.Visible = true;
+            this.gridColumn4.VisibleIndex = 4;
+            // 
+            // gridColumn5
+            // 
+            this.gridColumn5.Caption = "Bit 11";
+            this.gridColumn5.FieldName = "bit_11";
+            this.gridColumn5.Name = "gridColumn5";
+            this.gridColumn5.Visible = true;
+            this.gridColumn5.VisibleIndex = 5;
+            // 
+            // gridColumn6
+            // 
+            this.gridColumn6.Caption = "Bit 10";
+            this.gridColumn6.FieldName = "bit_10";
+            this.gridColumn6.Name = "gridColumn6";
+            this.gridColumn6.Visible = true;
+            this.gridColumn6.VisibleIndex = 6;
+            // 
+            // gridColumn7
+            // 
+            this.gridColumn7.Caption = "Bit 9";
+            this.gridColumn7.FieldName = "bit_9";
+            this.gridColumn7.Name = "gridColumn7";
+            this.gridColumn7.Visible = true;
+            this.gridColumn7.VisibleIndex = 7;
+            // 
+            // gridColumn8
+            // 
+            this.gridColumn8.Caption = "Bit 8";
+            this.gridColumn8.FieldName = "bit_8";
+            this.gridColumn8.Name = "gridColumn8";
+            this.gridColumn8.Visible = true;
+            this.gridColumn8.VisibleIndex = 8;
+            // 
+            // gridColumn9
+            // 
+            this.gridColumn9.Caption = "Bit 7";
+            this.gridColumn9.FieldName = "bit_7";
+            this.gridColumn9.Name = "gridColumn9";
+            this.gridColumn9.Visible = true;
+            this.gridColumn9.VisibleIndex = 9;
+            // 
+            // gridColumn10
+            // 
+            this.gridColumn10.Caption = "Bit 6";
+            this.gridColumn10.FieldName = "bit_6";
+            this.gridColumn10.Name = "gridColumn10";
+            this.gridColumn10.Visible = true;
+            this.gridColumn10.VisibleIndex = 10;
+            // 
+            // gridColumn11
+            // 
+            this.gridColumn11.Caption = "Bit 5";
+            this.gridColumn11.FieldName = "bit_5";
+            this.gridColumn11.Name = "gridColumn11";
+            this.gridColumn11.Visible = true;
+            this.gridColumn11.VisibleIndex = 11;
+            // 
+            // gridColumn12
+            // 
+            this.gridColumn12.Caption = "Bit 4";
+            this.gridColumn12.FieldName = "bit_4";
+            this.gridColumn12.Name = "gridColumn12";
+            this.gridColumn12.Visible = true;
+            this.gridColumn12.VisibleIndex = 12;
+            // 
+            // gridColumn13
+            // 
+            this.gridColumn13.Caption = "Bit 3";
+            this.gridColumn13.FieldName = "bit_3";
+            this.gridColumn13.Name = "gridColumn13";
+            this.gridColumn13.Visible = true;
+            this.gridColumn13.VisibleIndex = 13;
+            // 
+            // gridColumn14
+            // 
+            this.gridColumn14.Caption = "Bit 2";
+            this.gridColumn14.FieldName = "bit_2";
+            this.gridColumn14.Name = "gridColumn14";
+            this.gridColumn14.Visible = true;
+            this.gridColumn14.VisibleIndex = 14;
+            // 
+            // gridColumn15
+            // 
+            this.gridColumn15.Caption = "Bit 1";
+            this.gridColumn15.FieldName = "bit_1";
+            this.gridColumn15.Name = "gridColumn15";
+            this.gridColumn15.Visible = true;
+            this.gridColumn15.VisibleIndex = 15;
+            // 
+            // gridColumn16
+            // 
+            this.gridColumn16.Caption = "Bit 0";
+            this.gridColumn16.FieldName = "bit_0";
+            this.gridColumn16.Name = "gridColumn16";
+            this.gridColumn16.Visible = true;
+            this.gridColumn16.VisibleIndex = 16;
+            // 
+            // btn_reload
+            // 
+            this.btn_reload.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_reload.Location = new System.Drawing.Point(3, 3);
+            this.btn_reload.Name = "btn_reload";
+            this.btn_reload.Size = new System.Drawing.Size(794, 34);
+            this.btn_reload.TabIndex = 1;
+            this.btn_reload.Text = "Reload";
+            this.btn_reload.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.gc_main, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_reload, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 2;
             // 
             // frmPLCValueRealtime
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.gc_main);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "frmPLCValueRealtime";
             this.Text = "PLCValueRealtimeForm";
             this.Load += new System.EventHandler(this.frmPLCValueRealtime_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gc_main)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gv_main)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -70,5 +271,24 @@ namespace AdvancedHMICS
 
         private DevExpress.XtraGrid.GridControl gc_main;
         private DevExpress.XtraGrid.Views.Grid.GridView gv_main;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn3;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn4;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn5;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn6;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn7;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn8;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn9;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn10;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn11;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn12;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn13;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn14;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn15;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn16;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn17;
+        private System.Windows.Forms.Button btn_reload;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- AdvancedHMICS/Form/frmPLCValueRealtime.cs
+++ AdvancedHMICS/Form/frmPLCValueRealtime.cs
@@ -14,52 +14,46 @@ namespace AdvancedHMICS
 {
     public partial class frmPLCValueRealtime : Form
     {
-        public frmPLCValueRealtime()
+        public frmPLCValueRealtime(int[] D_)
         {
             InitializeComponent();
+            output_ = D_;
         }
         public int[] output_ = new int[8];
         DataTable dt;
-        public frmPLCValueRealtime(int[] D_)
-        {
-            output_ = D_;
-          
-        }
+
         private void frmPLCValueRealtime_Load(object sender, EventArgs e)
         {
             splitdata();
         }
-        void selectdata()
-        {
-            sqlite sqlite_ = new sqlite();
-            sqlite_.SelectData("select * from m_plcstatus", ref dt);
-        }
+
         void splitdata()
         {
-            dt = new DataTable();
-            Class.DataFuction data_ = new Class.DataFuction();
-            string a = data_.ToBinary(output_[0]);
-            selectdata();
-            DataRow row = dt.NewRow();
-            row["address_"] = "D0";
-            row["bit_01"] = 1;
-            row["bit_02"] = 1;
-            row["bit_03"] = 1;
-            row["bit_04"] = 1;
-            row["bit_05"] = 1;
-            row["bit_06"] = 1;
-            row["bit_07"] = 1;
-            row["bit_10"] = 1;
-            row["bit_11"] = 1;
-            row["bit_12"] = 1;
-            row["bit_13"] = 1;
-            row["bit_14"] = 0;
-            row["bit_15"] = 1;
-            row["bit_16"] = 1;
-            row["bit_17"] = 1;
-
-            dt.Rows.Add(row);
-            gc_main.DataSource = dt;
+            try
+            {
+                //khởi tạo table header
+                dt = new DataTable();
+                sqlite sqlite_ = new sqlite();
+                sqlite_.SelectData("select * from m_plcstatus", ref dt);
+                //insert into gridcontroll
+                Class.DataFuction data_ = new Class.DataFuction();
+                string nvalue = "";// data_.ToBinary(output_[0]);
+                for (int n = 0; n < 7; n++)
+                {
+                    nvalue = data_.ToBinary(output_[n]);
+                    DataRow row = dt.NewRow();
+                    row["address_"] = "D" + n;
+                    int[] split = new int[16];
+                    for (int i = 0; i < nvalue.Length; i++)
+                    {
+                        row["bit_" + (nvalue.Length - 1 - i).ToString()] = int.Parse(nvalue.Substring(i, 1));
+                    }
+                    dt.Rows.Add(row);
+                }
+                gc_main.DataSource = dt;
+            }
+            catch
+            { }
         }
     }
 }
