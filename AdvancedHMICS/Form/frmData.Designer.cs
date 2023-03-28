82b6a74b5f140ed005d6c75ac6cbfcfb5f7186e8
D-Khoa <ndkhoa081192@gmail.com>
2023-03-28 22:37:41 +0700

    Cập nhật giao diện truy vấn data

--- AdvancedHMICS/Form/frmData.Designer.cs
+++ AdvancedHMICS/Form/frmData.Designer.cs
@@ -39,6 +39,10 @@
             this.dtpToDate = new System.Windows.Forms.DateTimePicker();
             this.btnExport = new System.Windows.Forms.Button();
             this.btnRemove = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLot = new System.Windows.Forms.TextBox();
             this.tlpMain.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,15 +55,19 @@
             this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlpMain.Controls.Add(this.label3, 1, 0);
-            this.tlpMain.Controls.Add(this.cboModels, 1, 1);
-            this.tlpMain.Controls.Add(this.btnRefresh, 1, 2);
-            this.tlpMain.Controls.Add(this.cbFromDate, 2, 0);
-            this.tlpMain.Controls.Add(this.cbToDate, 3, 0);
-            this.tlpMain.Controls.Add(this.dtpFromDate, 2, 1);
-            this.tlpMain.Controls.Add(this.dtpToDate, 3, 1);
-            this.tlpMain.Controls.Add(this.btnExport, 2, 2);
-            this.tlpMain.Controls.Add(this.btnRemove, 3, 2);
+            this.tlpMain.Controls.Add(this.label3, 2, 0);
+            this.tlpMain.Controls.Add(this.cboModels, 2, 1);
+            this.tlpMain.Controls.Add(this.btnRefresh, 0, 2);
+            this.tlpMain.Controls.Add(this.cbFromDate, 3, 0);
+            this.tlpMain.Controls.Add(this.cbToDate, 4, 0);
+            this.tlpMain.Controls.Add(this.dtpFromDate, 3, 1);
+            this.tlpMain.Controls.Add(this.dtpToDate, 4, 1);
+            this.tlpMain.Controls.Add(this.btnExport, 1, 2);
+            this.tlpMain.Controls.Add(this.btnRemove, 4, 2);
+            this.tlpMain.Controls.Add(this.label1, 0, 0);
+            this.tlpMain.Controls.Add(this.txtBarcode, 0, 1);
+            this.tlpMain.Controls.Add(this.label2, 1, 0);
+            this.tlpMain.Controls.Add(this.txtLot, 1, 1);
             this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tlpMain.Location = new System.Drawing.Point(0, 0);
             this.tlpMain.Name = "tlpMain";
@@ -76,7 +84,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label3.Location = new System.Drawing.Point(163, 0);
+            this.label3.Location = new System.Drawing.Point(323, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(154, 23);
             this.label3.TabIndex = 3;
@@ -87,7 +95,7 @@
             // 
             this.cboModels.Dock = System.Windows.Forms.DockStyle.Fill;
             this.cboModels.FormattingEnabled = true;
-            this.cboModels.Location = new System.Drawing.Point(163, 26);
+            this.cboModels.Location = new System.Drawing.Point(323, 26);
             this.cboModels.Name = "cboModels";
             this.cboModels.Size = new System.Drawing.Size(154, 21);
             this.cboModels.TabIndex = 5;
@@ -98,7 +106,7 @@
             this.btnRefresh.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRefresh.Location = new System.Drawing.Point(163, 53);
+            this.btnRefresh.Location = new System.Drawing.Point(3, 53);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(154, 34);
             this.btnRefresh.TabIndex = 0;
@@ -112,7 +120,7 @@
             this.cbFromDate.Checked = true;
             this.cbFromDate.CheckState = System.Windows.Forms.CheckState.Checked;
             this.cbFromDate.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cbFromDate.Location = new System.Drawing.Point(323, 3);
+            this.cbFromDate.Location = new System.Drawing.Point(483, 3);
             this.cbFromDate.Name = "cbFromDate";
             this.cbFromDate.Size = new System.Drawing.Size(154, 17);
             this.cbFromDate.TabIndex = 6;
@@ -125,7 +133,7 @@
             this.cbToDate.Checked = true;
             this.cbToDate.CheckState = System.Windows.Forms.CheckState.Checked;
             this.cbToDate.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cbToDate.Location = new System.Drawing.Point(483, 3);
+            this.cbToDate.Location = new System.Drawing.Point(643, 3);
             this.cbToDate.Name = "cbToDate";
             this.cbToDate.Size = new System.Drawing.Size(154, 17);
             this.cbToDate.TabIndex = 7;
@@ -137,7 +145,7 @@
             this.dtpFromDate.CustomFormat = "yyyy-MM-dd HH:mm:ss";
             this.dtpFromDate.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dtpFromDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpFromDate.Location = new System.Drawing.Point(323, 26);
+            this.dtpFromDate.Location = new System.Drawing.Point(483, 26);
             this.dtpFromDate.Name = "dtpFromDate";
             this.dtpFromDate.Size = new System.Drawing.Size(154, 20);
             this.dtpFromDate.TabIndex = 8;
@@ -147,7 +155,7 @@
             this.dtpToDate.CustomFormat = "yyyy-MM-dd HH:mm:ss";
             this.dtpToDate.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dtpToDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpToDate.Location = new System.Drawing.Point(483, 26);
+            this.dtpToDate.Location = new System.Drawing.Point(643, 26);
             this.dtpToDate.Name = "dtpToDate";
             this.dtpToDate.Size = new System.Drawing.Size(154, 20);
             this.dtpToDate.TabIndex = 9;
@@ -158,13 +166,13 @@
             this.btnExport.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnExport.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExport.Location = new System.Drawing.Point(323, 53);
+            this.btnExport.Location = new System.Drawing.Point(163, 53);
             this.btnExport.Name = "btnExport";
             this.btnExport.Size = new System.Drawing.Size(154, 34);
             this.btnExport.TabIndex = 10;
             this.btnExport.Text = "Export";
             this.btnExport.UseVisualStyleBackColor = false;
-            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            this.btnExport.Click += new System.EventHandler(this.BtnExport_Click);
             // 
             // btnRemove
             // 
@@ -172,13 +180,51 @@
             this.btnRemove.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRemove.Location = new System.Drawing.Point(483, 53);
+            this.btnRemove.Location = new System.Drawing.Point(643, 53);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(154, 34);
             this.btnRemove.TabIndex = 11;
             this.btnRemove.Text = "Remove";
             this.btnRemove.UseVisualStyleBackColor = false;
-            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            this.btnRemove.Click += new System.EventHandler(this.BtnRemove_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 23);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Barcode:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtBarcode.Location = new System.Drawing.Point(3, 26);
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(154, 20);
+            this.txtBarcode.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(163, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 23);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Lot:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtLot
+            // 
+            this.txtLot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLot.Location = new System.Drawing.Point(163, 26);
+            this.txtLot.Name = "txtLot";
+            this.txtLot.Size = new System.Drawing.Size(154, 20);
+            this.txtLot.TabIndex = 15;
             // 
             // frmData
             // 
@@ -207,5 +253,9 @@
         private System.Windows.Forms.DateTimePicker dtpToDate;
         private System.Windows.Forms.Button btnExport;
         private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBarcode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLot;
     }
 }
--- AdvancedHMICS/Form/frmData.cs
+++ AdvancedHMICS/Form/frmData.cs
@@ -4,6 +4,8 @@ using DevExpress.XtraGrid.Views.Grid;
 using System;
 using System.Data;
 using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
@@ -17,6 +19,7 @@ namespace AdvancedHMICS
         public frmData()
         {
             InitializeComponent();
+            dtpFromDate.Value = dtpToDate.Value.Date;
             InitGrid();
         }
 
@@ -30,6 +33,7 @@ namespace AdvancedHMICS
             tlpMain.SetColumn(gc_main, 0);
             tlpMain.SetColumnSpan(gc_main, 6);
             tlpMain.Controls.Add(gc_main);
+            gv_main.CustomDrawCell += Gv_main_CustomDrawCell;
         }
 
         protected override void OnLoad(EventArgs e)
@@ -47,7 +51,7 @@ namespace AdvancedHMICS
             }
         }
 
-        private void BtnRefresh_Click(object sender, System.EventArgs e)
+        private void BtnRefresh_Click(object sender, EventArgs e)
         {
             try
             {
@@ -58,6 +62,10 @@ namespace AdvancedHMICS
                                            ck_barcode, ck_actual_power, ck_pid_stop, ck_rot_speed, 
                                            ck_fw_volt, ck_volt, ck_current, ck_result    
                                     FROM m_ck_point_data WHERE 1=1 ");
+                if (!string.IsNullOrWhiteSpace(txtBarcode.Text))
+                {
+                    sqlBuilder.AppendFormat("AND ck_barcode='{0}' ", txtBarcode.Text);
+                }
                 if (!string.IsNullOrWhiteSpace(cboModels.Text))
                 {
                     sqlBuilder.AppendFormat("AND ck_model='{0}' ", cboModels.Text);
@@ -73,6 +81,40 @@ namespace AdvancedHMICS
                 sqlite sqlite_ = new sqlite();
                 sqlite_.SelectData(sqlBuilder.ToString(), ref dt);
                 gc_main.DataSource = dt;
+                gv_main.Columns["ck_time"].Caption = "Time";
+                gv_main.Columns["ck_time"].VisibleIndex = 0;
+                gv_main.Columns["ck_model"].Caption = "Model";
+                gv_main.Columns["ck_model"].VisibleIndex = 1;
+                gv_main.Columns["ck_barcode"].Caption = "Barcode";
+                gv_main.Columns["ck_barcode"].VisibleIndex = 2;
+                gv_main.Columns["ck_power"].Caption = "Total Power";
+                gv_main.Columns["ck_power"].VisibleIndex = 3;
+                gv_main.Columns["ck_speed"].Caption = "Speed";
+                gv_main.Columns["ck_speed"].VisibleIndex = 4;
+                gv_main.Columns["ck_cycles"].Caption = "Cycles";
+                gv_main.Columns["ck_cycles"].VisibleIndex = 5;
+                gv_main.Columns["ck_serial"].Caption = "Step Id";
+                gv_main.Columns["ck_serial"].VisibleIndex = 6;
+                gv_main.Columns["ck_Steppercentage"].Caption = "Percentage";
+                gv_main.Columns["ck_Steppercentage"].VisibleIndex = 7;
+                gv_main.Columns["ck_Steppower"].Caption = "Step Power";
+                gv_main.Columns["ck_Steppower"].VisibleIndex = 8;
+                gv_main.Columns["ck_actual_power"].Caption = "Actual Power";
+                gv_main.Columns["ck_actual_power"].VisibleIndex = 9;
+                gv_main.Columns["ck_pid_stop"].Caption = "PID Stop";
+                gv_main.Columns["ck_pid_stop"].VisibleIndex = 10;
+                gv_main.Columns["ck_rot_speed"].Caption = "ROT Speed";
+                gv_main.Columns["ck_rot_speed"].VisibleIndex = 11;
+                gv_main.Columns["ck_fw_volt"].Caption = "FW Volt";
+                gv_main.Columns["ck_fw_volt"].VisibleIndex = 12;
+                gv_main.Columns["ck_volt"].Caption = "Volt";
+                gv_main.Columns["ck_volt"].VisibleIndex = 13;
+                gv_main.Columns["ck_current"].Caption = "Current";
+                gv_main.Columns["ck_current"].VisibleIndex = 14;
+                gv_main.Columns["ck_result"].Caption = "Result";
+                gv_main.Columns["ck_result"].VisibleIndex = 15;
+                gv_main.Columns["ck_testbrakes"].Caption = "Testbrakes";
+                gv_main.Columns["ck_testbrakes"].VisibleIndex = 16;
             }
             catch (Exception ex)
             {
@@ -80,7 +122,7 @@ namespace AdvancedHMICS
             }
         }
 
-        private void btnExport_Click(object sender, EventArgs e)
+        private void BtnExport_Click(object sender, EventArgs e)
         {
             try
             {
@@ -94,8 +136,34 @@ namespace AdvancedHMICS
             }
         }
 
-        private void btnRemove_Click(object sender, EventArgs e)
+        private void BtnRemove_Click(object sender, EventArgs e)
         {
+            if (MessageBox.Show("Do you want remove this rows?", "Caution",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning) != DialogResult.Yes)
+            {
+                return;
+            }
+            int[] rows = gv_main.GetSelectedRows();
+            StringBuilder sqlBuilder = new StringBuilder();
+            sqlBuilder.Append("DELETE FROM m_ck_point_data WHERE ID in (");
+            var ids = rows.Select(r => gv_main.GetRowCellValue(r, "ID")?.ToString());
+            sqlBuilder.Append(string.Join(",", ids));
+            sqlBuilder.Append(");");
+            sqlite sqlite_ = new sqlite();
+            sqlite_.exeNonQuery_auto(sqlBuilder.ToString());
+            MessageBox.Show("Remove data successed");
+            BtnRefresh_Click(sender, e);
+        }
+
+        private void Gv_main_CustomDrawCell(object sender, DevExpress.XtraGrid.Views.Base.RowCellCustomDrawEventArgs e)
+        {
+            GridView view = sender as GridView;
+            if (e.RowHandle >= 0)
+            {
+                string result = view.GetRowCellDisplayText(e.RowHandle, view.Columns["ck_result"]);
+                e.Appearance.BackColor = result == "1" ? Color.Green : Color.Red;
+                e.Appearance.ForeColor = Color.Yellow;
+            }
         }
     }
 }
